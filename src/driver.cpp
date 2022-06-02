a55e45f4f1900764e724a10349aa261bbfbab1c5
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2022-06-02 15:29:06 -0400

    Add simple driver
    This driver implements a 15x15 board and initializes a glider on it,
    then runs a render/update loop.

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,1 +1,27 @@
 #include "../lib/conway.hpp"
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+
+    int bSize = 15;
+    conway::LifeBoard b(bSize);
+
+    b.setCellState(0, 2, conway::ALIVE);
+    b.setCellState(1, 0, conway::ALIVE);
+    b.setCellState(2, 1, conway::ALIVE);
+    b.setCellState(2, 2, conway::ALIVE);
+    b.setCellState(1, 2, conway::ALIVE);
+
+    int clock = 0;
+    while(clock < 10000000000) {
+        cout << '\n';
+        b.renderBoard();
+        b.updateBoard();
+        usleep(280000);
+    }
+
+    return 0;
+}
